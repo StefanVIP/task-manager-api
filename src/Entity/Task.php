3805c8536a98838a19187bf1bbf102dba322588f
Stepan <steptarget@yandex.ru>
2023-09-21 04:14:47 +0700

    Correct Task Entity

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -2,22 +2,28 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\Delete;
 use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
-//#[UniqueEntity('slug')]
 #[ApiResource(
-//    operations: [
-//                new Get(normalizationContext: ['groups' => 'task:item']),
-//                new GetCollection(normalizationContext: ['groups' => 'task:list'])
-//            ],
+    operations: [
+                new Get(),
+                new GetCollection(),
+                new Post(denormalizationContext: ['groups' => 'task:write']),
+                new Put(denormalizationContext: ['groups' => 'task:write']),
+                new Patch(uriTemplate: '/tasks/{id}/done', denormalizationContext: ['groups' => 'task:status']),
+                new Delete()
+            ],
         order: ['createDate' => 'ASC'],
     paginationEnabled: true,
 )]
@@ -29,12 +35,12 @@ class Task
     #[Groups(['task:list', 'task:item'])]
     private ?int $id = null;
 
-    #[ORM\Column(length: 255)]
-    #[Groups(['task:list', 'task:item'])]
+    #[ORM\Column(type: Types::STRING, length: 255)]
+    #[Groups(['task:list', 'task:item', 'task:write'])]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups(['task:list', 'task:item'])]
+    #[Groups(['task:list', 'task:item', 'task:write'])]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DATETIME_IMMUTABLE)]
@@ -42,12 +48,12 @@ class Task
     private ?\DateTimeImmutable $createDate;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
-    #[Groups(['task:list', 'task:item'])]
+    #[Groups(['task:list', 'task:item', 'task:write'])]
     private ?\DateTimeInterface $completeDate = null;
 
-    #[ORM\Column]
-    #[Groups(['task:list', 'task:item'])]
-    private ?bool $status = false;
+    #[ORM\Column(type: Types::BOOLEAN)]
+    #[Groups(['task:list', 'task:item', 'task:status'])]
+    private ?bool $status;
 
     public function getId(): ?int
     {
@@ -83,11 +89,10 @@ class Task
         return $this->createDate;
     }
 
-    public function setCreateDate(\DateTimeInterface $createDate): static
+    public function __construct()
     {
-        $this->createDate = $createDate;
-
-        return $this;
+        $this->createDate = new \DateTimeImmutable();
+        $this->status = false;
     }
 
     public function getCompleteDate(): ?\DateTimeInterface
@@ -102,12 +107,12 @@ class Task
         return $this;
     }
 
-    public function isStatus(): ?bool
+    public function getStatus(): ?bool
     {
         return $this->status;
     }
 
-    public function setStatus(bool $status): static
+    public function setStatus(bool $status = true): static
     {
         $this->status = $status;
 
